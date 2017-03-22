ed16e8ccdffc6ea29f95cfb37b45727a1ec65776
Alex Totheroh <atothero@indiana.edu>
2017-03-21 21:26:18 -0400

    properly reading from standard input

---
+++ TheCoinChangeProblem/src/scala/main/Solution.scala
@@ -0,0 +1,18 @@
+object Solution {
+
+  import io.StdIn.readLine
+
+  def main(args: Array[String]): Unit = {
+
+    // Read input from standard input
+    println("Enter two integers separated by a space.  The first is the value you want the coins to add up to.  The second is the number of different coins, each infinitely available.")
+    val valueAndCoinListLengthText = readLine()
+    println("Enter the integer values for each coin separated by a space.")
+    val coinsText = readLine()
+
+    // Get the inputs in a more easily usable form
+
+
+  }
+
+}
