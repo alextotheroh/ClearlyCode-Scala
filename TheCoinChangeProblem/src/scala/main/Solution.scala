e215f0612ea9ca7cb9fd4fd73b6edd9f5e5652ef
Alex Totheroh <alextotheroh@gmail.com>
2017-04-03 21:54:37 -0400

    more setup

--- TheCoinChangeProblem/src/scala/main/Solution.scala
+++ TheCoinChangeProblem/src/scala/main/Solution.scala
@@ -4,13 +4,29 @@ object Solution {
 
   def main(args: Array[String]): Unit = {
 
-    // Read input from standard input
+
+    // Read input from standard input:
+
     println("Enter two integers separated by a space.  The first is the value you want the coins to add up to.  The second is the number of different coins, each infinitely available.")
     val valueAndCoinListLengthText = readLine()
     println("Enter the integer values for each coin separated by a space.")
     val coinsText = readLine()
 
-    // Get the inputs in a more easily usable form
+    // Get the inputs in a more easily usable form:
+
+    // integer
+    val goal = valueAndCoinListLengthText.split(" ").head.toInt
+
+    // integer
+    val numberOfCoins = valueAndCoinListLengthText.split(" ").last.toInt
+
+    // array of integers representing coin values
+    val coins = coinsText.split(" ").map(coinString => coinString.toInt)
+
+    //println("goal: " + goal)
+    //println("numberOfCoins: " + numberOfCoins)
+    //println("coins: " + coins.mkString(" "))
+
 
 
   }
