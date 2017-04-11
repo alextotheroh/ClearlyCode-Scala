cf3d6e568a4f2cd56fd84f7598cf3004a9489b7b
alextotheroh <alextotheroh@gmail.com>
2017-04-10 22:11:32 -0400

    problem solved

--- Algorithms/DynamicProgramming/Candies/src/main/scala/Solution.scala
+++ Algorithms/DynamicProgramming/Candies/src/main/scala/Solution.scala
@@ -2,14 +2,34 @@ object Solution {
 
   def main(args: Array[String]): Unit ={
     val numberOfChildren: Int = scala.io.StdIn.readInt()
-    val ratings: Array[Int] = Array.fill(numberOfChildren)(0)
+
+    // we'll keep one array for the ratings and one for the
+    // number of candies per child
+    val ratings: Array[Long] = Array.fill(numberOfChildren)(0.toLong)
+    val candies: Array[Long] = Array.fill(numberOfChildren)(1.toLong)
 
     for (i <- 1 to numberOfChildren) {
       ratings(i-1) = scala.io.StdIn.readInt()
     }
 
-    //verify
-    //println(ratings.mkString(" "))
+    // We can solve this one by looping through the ratings
+    // twice: once forward and once backward, setting the
+    // number of candies appropriately
+
+    for (i <- 1 to numberOfChildren-1) {
+      if (ratings(i) > ratings(i-1)) {
+        candies(i) = candies(i-1)+1
+      }
+    }
+
+    for (j <- (0 to numberOfChildren-2).reverse) {
+      if (ratings(j) > ratings(j+1)) {
+        candies(j) = Math.max(candies(j), candies(j+1)+1)
+      }
+    }
+
+    println(candies.sum)
+
   }
 
 }
